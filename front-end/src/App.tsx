f0e85f066dd9958a69fd3fb4414e1223941bff64
missfionajean <missfionajean@gmail.com>
2025-12-11 16:36:14 -0500

    set up dev environment for custom calendar component; added placeholder title styling for tab

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
-import Dashboard from './components/Dashboard'
+import "./App.css";
+import Calendar from "./components/Calendar";
+import Dashboard from "./components/Dashboard";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Dashboard />
-    </>
-  )
+    const [view, setView] = useState("dashboard");
+	return (
+		<>
+        { view == "dashboard" ? <Dashboard /> : ""}
+        { view == "calendar" ? <Calendar /> : ""}
+        <button onClick={() => setView("dashboard")}>Dash</button>
+        <button onClick={() => setView("calendar")}>Cal</button>
+		</>
+	);
 }
 
-export default App
+export default App;
--- front-end/src/components/Calendar.tsx
+++ front-end/src/components/Calendar.tsx
@@ -2,3 +2,12 @@
 // make sure to properly lift states so that the components can affect each other
 // re-render calendar only when an update happens (put in logic to not do anything if no changes are made and submit is clicked)
 // make sure there's also loading states and progress trackers (spinning wheel, progress bar, etc.) for when API calls are made
+
+export default function Calendar() {
+    return (
+        <>
+            <h1>Calendar</h1>
+            {/* Add your calendar logic here */}
+        </>
+    );
+}
